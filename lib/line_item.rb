d6dcf7b1571318bd6affd6010f223a3b43c144fb
Joao P B <joaopb@MacBook-Pro-de-Paulo.local>
2024-12-04 20:25:13 -0300

    ensures that when line items are created, they are all not discounted

--- lib/line_item.rb
+++ lib/line_item.rb
@@ -1,11 +1,15 @@
 module Superfiliate
   class LineItem
     attr_reader :name, :price, :sku
-    
+
     def initialize(name, price, sku)
       @name = name
       @price = price
       @sku = sku
+    end
+
+    def discounted?
+      false
     end
   end
 end
--- test/cart_test.rb
+++ test/cart_test.rb
@@ -1,8 +1,20 @@
 require "test_helper"
 
 class Superfiliate::CartTest < Minitest::Test
+  def setup
+    @line_items = [
+      Superfiliate::LineItem.new("Peanut Butter", 3000, "PEANUTS"),
+      Superfiliate::LineItem.new("Chocolate", 2000, "CHOCOLATE")
+    ]
+  end
+
   def test_create_with_items
-    line_items = [ Superfiliate::LineItem.new("Peanut Butter", 3000, "PEANUTS"), Superfiliate::LineItem.new("Chocolate", 2000, "CHOCOLATE") ]
-    assert_equal Superfiliate::Cart.new(line_items).line_items.size, 2
+    cart = Superfiliate::Cart.new @line_items
+    assert_equal cart.line_items.size, 2
+  end
+
+  def test_when_cart_is_created_should_not_have_any_discounts_applied
+    cart = Superfiliate::Cart.new @line_items
+    assert cart.line_items.filter(&:discounted?).empty?
   end
 end
